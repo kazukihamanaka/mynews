77c5516386a06dea0f87e93e942212f3de2dd157
kazukihamanaka <kazuki.s1987@gmail.com>
2020-05-05 16:05:35 +0900

    Question

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
